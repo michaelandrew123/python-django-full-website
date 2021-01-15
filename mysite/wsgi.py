bf9d808b888a540739b5af9757d3db6827fa2b82
Michael Andrew Suarez <michaelandrewsuarez0@gmail.com>
2021-01-15 23:18:03 +0800

    Testing Update to online website

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+# import sys
 
 from django.core.wsgi import get_wsgi_application
 
+
+#path = '/home/suarezmike129/python-django-full-website'
+
+#if path not in sys.path:
+#    sys.path.insert(0, path)
+
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
